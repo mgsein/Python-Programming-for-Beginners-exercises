1244583e5b8d5315b3fe89bd09245ca6ebd0ade7
Chow Wain Sein @ Mg Aung Hein <tommysnow.01@gmail.com>
2025-11-05 21:31:00 +0700

    added web scraping files

---
+++ 6. Web Scraping/5. wikipedia.py
@@ -0,0 +1,26 @@
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from article_crawler.items import ArticleCrawlerItem
+
+class WikipediaSpider(CrawlSpider):
+    name = 'wikipedia'
+    allowed_domains = ['en.wikipedia.org']
+    start_urls = ['https://en.wikipedia.org/wiki/Kevin_Bacon']
+
+    rules = [
+        Rule(LinkExtractor(allow=r'wiki/((?!:).)*$'), callback='parse', follow=True)
+    ]
+
+    custom_settings={
+        'FEED_URI': 'articles.xml',
+        'FEED_FORMAT': 'xml'
+    }
+
+    def parse(self, response):
+        article = ArticleCrawlerItem()
+        article['title']= response.xpath('//h1/span/text()').get() or response.xpath('//h1/i/text()')
+        article['url'] = response.url
+
+        article['lastUpdated'] = response.xpath('//li[@id="footer-info-lastmod"]/text()').get()
+        return article
---
+++ 6. Web Scraping/6. get_form.py
@@ -0,0 +1,14 @@
+import scrapy
+
+def generate_start_urls():
+    names = ['Alice', 'Bob', 'Charles']
+    quests = ['to seek the grail', 'to learn python', 'to scrape the web']
+    return ['http://pythonscraping.com/linkedin/formAction.php?name={}&quest={}&color=blue'.format(name, quest.replace(' ', '%20')) for name in names for quest in quests]
+
+class GetFormSpider(scrapy.Spider):
+    name = 'get_form'
+    allowed_domains = ['pythonscraping.com']
+    start_urls = generate_start_urls()
+
+    def parse(self, response):
+        return {'text': response.xpath('//div[@class="wrapper"]/text()').get()}
