06bf6cd29677e0d46da885b67f47fc7d2fc7f54c
Chow Wain Sein @ Mg Aung Hein <tommysnow.01@gmail.com>
2025-11-05 21:42:01 +0700

    Update 4. apiKey.py

--- 7. Automation with Python/4. apiKey.py
+++ 7. Automation with Python/4. apiKey.py
@@ -1,5 +1,5 @@
 import requests
 baseUrl = 'http://api.openweathermap.org/data/2.5/forecast'
-parameters = {'lat': '16.871311', 'lon' : '96.199379', 'appid': '09cf540c9793fe97520c2bdbc81340ca'}
+parameters = {'lat': '16.871311', 'lon' : '96.199379', 'appid': ''}
 response = requests.get(baseUrl, params = parameters)
 print(response.content)
